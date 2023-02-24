fd9549c1f8f0710a06c908d82d2fc0ffae5d0381
Max Clifford <me@mclifford.dev>
2023-02-24 23:00:48 +0000

    Switched bytesToHex to use built method

--- FsToolbox/Core/Strings.fs
+++ FsToolbox/Core/Strings.fs
@@ -6,10 +6,14 @@ module Strings =
     open System.Text
 
     let bytesToHex (bytes: byte array) =
+        Convert.ToHexString bytes
+        
+        (*
         bytes
         |> Array.fold (fun (sb: StringBuilder) b -> sb.AppendFormat("{0:x2}", b)) (StringBuilder(bytes.Length * 2))
         |> fun sb -> sb.ToString()
-
+        *)
+    
     let equalOrdinal a b = String.Equals(a, b, StringComparison.Ordinal)
     
     let equalOrdinalIgnoreCase a b = String.Equals(a, b, StringComparison.OrdinalIgnoreCase)
