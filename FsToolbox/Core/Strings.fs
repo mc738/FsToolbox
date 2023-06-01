fd4dd6ca62ec4579ab330755089a42b5cefbd8cb
Max Clifford <me@mclifford.dev>
2023-06-01 18:43:42 +0100

    Added String.fromOptional and String.toOptional

--- FsToolbox/Core/Strings.fs
+++ FsToolbox/Core/Strings.fs
@@ -1,20 +1,30 @@
 ﻿namespace FsToolbox.Core
 
 module Strings =
-    
+
     open System
     open System.Text
 
-    let bytesToHex (bytes: byte array) =
-        Convert.ToHexString bytes
-        
-        (*
+    let bytesToHex (bytes: byte array) = Convert.ToHexString bytes
+
+    (*
         bytes
         |> Array.fold (fun (sb: StringBuilder) b -> sb.AppendFormat("{0:x2}", b)) (StringBuilder(bytes.Length * 2))
         |> fun sb -> sb.ToString()
         *)
-    
-    let equalOrdinal a b = String.Equals(a, b, StringComparison.Ordinal)
-    
-    let equalOrdinalIgnoreCase a b = String.Equals(a, b, StringComparison.OrdinalIgnoreCase)
 
+    let equalOrdinal a b =
+        String.Equals(a, b, StringComparison.Ordinal)
+
+    let equalOrdinalIgnoreCase a b =
+        String.Equals(a, b, StringComparison.OrdinalIgnoreCase)
+
+    let toOptional (str: string) =
+        match String.IsNullOrWhiteSpace str with
+        | true -> None
+        | false -> Some str
+
+    let fromOptional (str: string option) =
+        match str with
+        | Some v -> v
+        | None -> String.Empty
