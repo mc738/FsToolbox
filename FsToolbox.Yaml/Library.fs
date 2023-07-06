446551251dd3509f41520842a82a93043c352530
Max Clifford <me@mclifford.dev>
2023-07-06 20:12:43 +0100

    Added FsToolbox.Yaml

---
+++ FsToolbox.Yaml/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace FsToolbox.Yaml
+
+open YamlDotNet.RepresentationModel
+
+module Say =
+
+
+    let getValue (i: YamlScalarNode) =
+        ()
+
+    let test (i: YamlNode) =
+        
+        
+        match i.NodeType with
+        | YamlNodeType.Scalar ->
+            
+            
+            ""
+
+    let hello name =
+        printfn "Hello %s" name
