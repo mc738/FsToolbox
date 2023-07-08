a684e700110766a10d88dfd5c22c64ae9fc07550
Max Clifford <me@mclifford.dev>
2023-07-08 12:48:16 +0100

    Added Conversion.fs to Yaml

---
+++ FsToolbox.Yaml/Conversion.fs
@@ -0,0 +1,28 @@
+﻿namespace FsToolbox.Yaml
+
+open System.Text.Json
+open YamlDotNet.RepresentationModel
+
+module Conversion =
+    
+    
+    let jsonElementToYamlNode (element: JsonElement) =
+        match element.ValueKind with
+        | JsonValueKind.Array -> ()
+        | JsonValueKind.Undefined -> failwith "todo"
+        | JsonValueKind.Object -> failwith "todo"
+        | JsonValueKind.String ->
+            
+            YamlScalarNode()
+            
+            failwith "todo"
+        | JsonValueKind.Number -> failwith "todo"
+        | JsonValueKind.True -> failwith "todo"
+        | JsonValueKind.False -> failwith "todo"
+        | JsonValueKind.Null -> failwith "todo"
+        
+        
+    
+    
+    ()
+
