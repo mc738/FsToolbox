689f84e6a0200e8ad516241adbeef73e31272f93
Max Clifford <me@mclifford.dev>
2023-07-08 14:49:14 +0100

    Work on jsonElementToYamlNode

--- FsToolbox.Yaml/Conversion.fs
+++ FsToolbox.Yaml/Conversion.fs
@@ -1,28 +1,31 @@
 ﻿namespace FsToolbox.Yaml
 
 open System.Text.Json
+open FsToolbox.Core
 open YamlDotNet.RepresentationModel
 
 module Conversion =
-    
-    
-    let jsonElementToYamlNode (element: JsonElement) =
-        match element.ValueKind with
-        | JsonValueKind.Array -> ()
-        | JsonValueKind.Undefined -> failwith "todo"
-        | JsonValueKind.Object -> failwith "todo"
-        | JsonValueKind.String ->
-            
-            YamlScalarNode()
-            
-            failwith "todo"
-        | JsonValueKind.Number -> failwith "todo"
-        | JsonValueKind.True -> failwith "todo"
-        | JsonValueKind.False -> failwith "todo"
-        | JsonValueKind.Null -> failwith "todo"
-        
-        
-    
-    
-    ()
 
+
+    let jsonElementToYamlNode (element: JsonElement) =
+        let rec handler (el: JsonElement) =
+            match element.ValueKind with
+            | JsonValueKind.Array ->
+                element.EnumerateArray()
+                |> List.ofSeq
+                // NOTE Is List.choose correct here? Would it be better to fail on an error?
+                |> List.choose handler
+                |> fun els -> YamlSequenceNode(els |> Seq.ofList) :> YamlNode |> Some
+            | JsonValueKind.Undefined -> None
+            | JsonValueKind.Object -> failwith "todo"
+            | JsonValueKind.String -> Json.tryGetString el |> Option.map (fun s -> YamlScalarNode s :> YamlNode)
+            | JsonValueKind.Number -> failwith "todo"
+            | JsonValueKind.True -> failwith "todo"
+            | JsonValueKind.False -> failwith "todo"
+            | JsonValueKind.Null -> failwith "todo"
+
+        handler element
+
+
+
+    ()
