7ca064b5afe43f799bdd07f7146b3e63a14c566c
Max Clifford <me@mclifford.dev>
2023-07-09 10:14:16 +0100

    Work on json to yaml Conversion.fs

--- FsToolbox.Yaml/Conversion.fs
+++ FsToolbox.Yaml/Conversion.fs
@@ -1,5 +1,6 @@
 ﻿namespace FsToolbox.Yaml
 
+open System.Collections.Generic
 open System.Text.Json
 open FsToolbox.Core
 open YamlDotNet.RepresentationModel
@@ -11,15 +12,26 @@ module Conversion =
         let rec handler (el: JsonElement) =
             match element.ValueKind with
             | JsonValueKind.Array ->
-                element.EnumerateArray()
+                el.EnumerateArray()
                 |> List.ofSeq
                 // NOTE Is List.choose correct here? Would it be better to fail on an error?
                 |> List.choose handler
                 |> fun els -> YamlSequenceNode(els |> Seq.ofList) :> YamlNode |> Some
             | JsonValueKind.Undefined -> None
-            | JsonValueKind.Object -> failwith "todo"
+            | JsonValueKind.Object ->
+                el.EnumerateObject()
+                |> List.ofSeq 
+                |> List.choose (fun jp ->
+                    handler jp.Value
+                    |> Option.map (fun yn ->
+                        KeyValuePair<YamlNode, YamlNode>(YamlScalarNode(jp.Name), yn)))
+                |> YamlMappingNode
+                |> fun n -> n :> YamlNode |> Some
             | JsonValueKind.String -> Json.tryGetString el |> Option.map (fun s -> YamlScalarNode s :> YamlNode)
-            | JsonValueKind.Number -> failwith "todo"
+            | JsonValueKind.Number ->
+                // 
+                
+                failwith "todo"
             | JsonValueKind.True -> failwith "todo"
             | JsonValueKind.False -> failwith "todo"
             | JsonValueKind.Null -> failwith "todo"
