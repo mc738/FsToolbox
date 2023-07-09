980b15729286e2d093571e2a435593bb7183fe1e
Max Clifford <me@mclifford.dev>
2023-07-09 10:20:55 +0100

    Added FsToolbox.Yaml.Tests

---
+++ FsToolbox.Yaml.Tests/Conversions.fs
@@ -0,0 +1,19 @@
+﻿namespace FsToolbox.Yaml.Tests
+
+open System.Collections.Generic
+open System.Text.Json
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open YamlDotNet.RepresentationModel
+
+[<TestClass>]
+type ConversionTests() =
+
+    [<TestMethod>]
+    member _.``Json object to yaml``() =
+        let obj = JsonDocument.Parse("{ ").RootElement
+
+        let expected =
+            YamlMappingNode(seq { KeyValuePair(YamlScalarNode "test", YamlScalarNode "Hello, World!") })
+        
+
+        ()
