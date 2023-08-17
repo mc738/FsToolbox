8c6da12ce0ae725e3b7161b835e0b0c9e1ad0dfb
Max Clifford <me@mclifford.dev>
2023-08-17 18:58:23 +0100

    Added FromBase64String

--- FsToolbox/Extensions/Streams.fs
+++ FsToolbox/Extensions/Streams.fs
@@ -12,6 +12,9 @@ module Streams =
 
         static member FromUtf8String(str: string) = new MemoryStream(str |> Encoding.UTF8.GetBytes)
         
+        static member FromBase64String(str: string) =
+            new MemoryStream(str |> Conversions.fromBase64)
+            
         member ms.GetSHA256Hash() = Hashing.hashStream (SHA256.Create()) ms
         
         member ms.GetUtf8String() = ms.ToArray() |> Encoding.UTF8.GetString
