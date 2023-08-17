f029af94345118ad6b2fb10fc6b7fe0d3e598262
Max Clifford <me@mclifford.dev>
2023-08-17 18:50:13 +0100

    Added FromUtf8String() extension

--- FsToolbox/Extensions/Streams.fs
+++ FsToolbox/Extensions/Streams.fs
@@ -10,6 +10,10 @@ module Streams =
         
     type MemoryStream with
 
+        static member FromUtf8String(str: string) = new MemoryStream(str |> Encoding.UTF8.GetBytes)
+        
         member ms.GetSHA256Hash() = Hashing.hashStream (SHA256.Create()) ms
         
         member ms.GetUtf8String() = ms.ToArray() |> Encoding.UTF8.GetString
+        
+        
