78185ad845a4ac8efb2ab6526aa0cf2d5ccbf99c
max <me@mclifford.dev>
2024-10-17 22:43:24 +0100

    Work on Option.fs

--- FsToolbox/Core/Option.fs
+++ FsToolbox/Core/Option.fs
@@ -3,8 +3,6 @@ namespace FsToolbox.Core
 [<RequireQualifiedAccess>]
 module Option =
 
-    
-    
     let ifTrue<'TIn, 'TResult> (value: 'TResult) (option: bool option) =
         match option with
         | Some true -> Some value
@@ -17,6 +15,28 @@ module Option =
         | Some false
         | None -> None
         
-    let bindIf
+    let bindIfTrue<'TIn, 'TResult> (whenTrueTrunk: unit -> 'TResult option) (option: bool option) =
+        match option with
+        | Some true -> whenTrueTrunk ()
+        | Some false
+        | None -> None
+        
+    let ifFalse<'TIn, 'TResult> (value: 'TResult) (option: bool option) =
+        match option with
+        | Some false -> Some value
+        | Some true 
+        | None -> None
+
+    let mapIfFalse<'TIn, 'TResult> (whenTrueTrunk: unit -> 'TResult) (option: bool option) =
+        match option with
+        | Some false -> whenTrueTrunk () |> Some
+        | Some true 
+        | None -> None
+        
+    let bindIfFalse<'TIn, 'TResult> (whenTrueTrunk: unit -> 'TResult option) (option: bool option) =
+        match option with
+        | Some false -> whenTrueTrunk ()
+        | Some true 
+        | None -> None
 
     ()
