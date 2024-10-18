df0cf5960ebb2cf07bc6e23bae98bfc26c783636
max <me@mclifford.dev>
2024-10-18 21:47:18 +0100

    Work on ProcessWrappers

---
+++ FsToolbox.ProcessWrappers/Common.fs
@@ -0,0 +1,16 @@
+namespace FsToolbox.ProcessWrappers
+
+[<AutoOpen>]
+module Common =
+    
+    let wrapString (str: string) =
+        match str.Contains(' ') with
+        | true -> $"\"{str}\""
+        | false -> str
+        
+    let forceWrapString (str: string) = $"\"{str}\""
+
+    let concatStrings (separator: string) (values: string seq) =
+        values
+        |> Seq.map wrapString
+        |> String.concat separator
