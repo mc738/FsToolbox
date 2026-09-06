70097ef2c7dd2a7081e72a2c896dfab41589a4d3
Max <me@mclifford.dev>
2026-09-06 21:55:49 +0100

    chore(core): Work on Materials.fs

--- FsToolbox.OpenGL/Materials.fs
+++ FsToolbox.OpenGL/Materials.fs
@@ -4,18 +4,17 @@ open System.Numerics
 open FsToolbox.OpenGL.Shaders
 
 [<AbstractClass>]
-type OpenGLMaterial(shader: OpenGLShader) =
+type OpenGLMaterial(shader: OpenGLShader) as this =
     
-    abstract member Bind: View: Matrix4x4 * Projection: Matrix4x4 * ModelMatrix: Matrix4x4 -> unit
+    /// An abstract method that will be called each time a model is bound.
+    abstract member OnModelBind: unit -> unit
         
     member _.Use() = shader.Use()
     
     member _.BindViewProjection(view: Matrix4x4, projection: Matrix4x4) =
-        
         shader.SetUniform("uView", view)
         shader.SetUniform("uProjection", projection)
-        ()
     
     member _.BindModel(modelMatrix: Matrix4x4) =
         shader.SetUniform("uModel", modelMatrix)
-        ()
+        this.OnModelBind()
